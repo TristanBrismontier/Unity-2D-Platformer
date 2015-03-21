0091d49618652ad78c0e8c7dbc68eadfedbd091c
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-03-21 17:56:43 +0100

    Astroid

---
+++ space-shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	public Rigidbody rb;
+	public float speed;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = transform.forward * speed;
+	}
+}
--- space-shooter/Assets/Scripts/PlayerController.cs
+++ space-shooter/Assets/Scripts/PlayerController.cs
@@ -10,7 +10,14 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
-	public Rigidbody rb;
+	private Rigidbody rb;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	public float fireRate;
+	private float nextFire = 0.0F;
+
 	void Start() {
 		rb = GetComponent<Rigidbody>();
 	}
@@ -30,5 +37,12 @@ public class PlayerController : MonoBehaviour {
 				Mathf.Clamp(rb.position.z,boundary.zMin,boundary.zMax)
 			);
 		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * tilt);
+	}
+
+	void Update (){
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate(shot,shotSpawn.position,shotSpawn.rotation);
+		}
 	}
 }
