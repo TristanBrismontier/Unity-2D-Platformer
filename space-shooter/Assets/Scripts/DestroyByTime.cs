5c727e4529f98aa1174a15d7403444121fe9fc52
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-03-22 01:25:53 +0100

    Add Waves

---
+++ space-shooter/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public float lifeTime;
+	void Start () {
+		Destroy (gameObject, lifeTime);
+	}
+}
---
+++ space-shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValue;
+	public int hazardCount;
+	public float spawnWait;
+	public float stratWait;
+	public float waveWait;
+
+	void Start () {
+		StartCoroutine (SpawnWaves());
+	}
+
+	IEnumerator SpawnWaves () {
+		yield return new WaitForSeconds(spawnWait);
+		while (true) {
+			for (int i = 0; i<hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
+	}
+}
