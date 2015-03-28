cc55d2cf8bc878c896dbba4585870ec5516300e0
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-03-28 15:50:07 +0100

    Initialize player

---
+++ Rogue-Like/Space-Invader/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical); 
+		rb.velocity = movement * speed;
+	}
+	
+	void Update (){
+	}
+}
