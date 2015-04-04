d70ad08cd5a3a3142cd9dd20dba36df367e2a20b
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-04-04 19:46:50 +0200

    Organize

--- space-shooter/Assets/Scripts/DestroyByTime.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class NewBehaviourScript : MonoBehaviour {
-	public float lifeTime;
-	void Start () {
-		Destroy (gameObject, lifeTime);
-	}
-}
