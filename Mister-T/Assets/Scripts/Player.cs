1ef9363ac91bac8be546a96d952766c58605cc80
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-04-08 12:33:03 +0200

    Set StartPosition

--- Mister-T/Assets/Scripts/Player.cs
+++ Mister-T/Assets/Scripts/Player.cs
@@ -3,25 +3,24 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	public float speed;
-
+	public Transform startPosition;
+	
 	private Rigidbody2D rb;
 	private Vector3 start;
 
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
-		start = new Vector3(0.5f,0,0);
 	}
 	void FixedUpdate(){
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f); 
 		transform.position += movement * speed * Time.deltaTime;
-		//rb.velocity = movement * speed;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if(transform.position.y < -1){
-			transform.position = start;
+			transform.position = startPosition.position;
 		}
 	
 	}
