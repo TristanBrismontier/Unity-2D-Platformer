b6d4102223cf5b693f0866c6d6cdf8045c32853f
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-05-01 20:02:10 +0200

    CubeSystem

--- Mister-T/Assets/Scripts/CubeSystem.cs
+++ Mister-T/Assets/Scripts/CubeSystem.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class CubeSystem : MonoBehaviour {
 
-	// Use this for initialization
+	public int columns;
+	public int rows;
+	public GameObject cube;
+
 	void Start () {
-	
+		CubeSystemSetup();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void CubeSystemSetup(){
+		for (float x=0; x<columns; x++) {
+			for(float y=0; y<rows; y++){
+				GameObject toInstantiate = cube;
+				GameObject instance = Instantiate(toInstantiate,new Vector3(x*0.1f+transform.position.x,y*0.1f+transform.position.y,0f),Quaternion.identity) as GameObject;
+				
+			}
+		}
 	}
+
 }
