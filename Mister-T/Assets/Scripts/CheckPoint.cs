b17cbc5712c597e1efdc505986342dace27e9eee
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-05-01 19:51:49 +0200

    CubeSystem

--- Mister-T/Assets/Scripts/CheckPoint.cs
+++ Mister-T/Assets/Scripts/CheckPoint.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class CheckPoint : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player"){
+			Debug.Log("CheckPoint");
+			GameManager.instance.startPosition = transform;
+		}
 	}
 }
