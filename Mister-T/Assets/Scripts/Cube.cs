fb11293af3d0567084077d853e1808eab9bdb935
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-05-01 20:10:59 +0200

    Cube Scripts

---
+++ Mister-T/Assets/Scripts/Cube.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour {
+
+	private Transform startPosition;
+	// Use this for initialization
+	void Start () {
+		startPosition = transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = transform.position.x;
+		float y = transform.position.y;
+
+		if( x < 0 || x > 100 || y > 10 || y < -2 ){
+			transform = startPosition;
+		}
+	}
+}
