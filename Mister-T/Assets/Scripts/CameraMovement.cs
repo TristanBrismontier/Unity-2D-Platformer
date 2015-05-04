ad3dca5f133bc1144b52a9f17ac4932551555223
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-05-04 21:02:19 +0200

    Fix Camera Movment

--- Mister-T/Assets/Scripts/CameraMovement.cs
+++ Mister-T/Assets/Scripts/CameraMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour {
 	
-	public float dampTime = 0.15f;
+	public float dampTime = 0.02f;
 	private Vector3 velocity = Vector3.zero;
 	public Transform target;
 	public GameObject[] hearts; 
@@ -24,8 +24,9 @@ public class CameraMovement : MonoBehaviour {
 			Vector3 delta = target.position - GetComponent<UnityEngine.Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
 			Vector3 destination = transform.position + delta;
 			destination = new Vector3(destination.x, Mathf.Clamp(destination.y,1.9f,100),destination.z);
+			transform.position = new Vector3(destination.x, transform.position.y,transform.position.z);
 			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+	
 		}
-		
 	}
 }
